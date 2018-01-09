1ce7b4840bb1de58263570e867f1af182f24beb8
swacademy <blueexpert@hotmail.com>
2018-01-10 06:14:32 +0900

    Add files via upload

---
+++ Chapter 4. Expressions and Operators/ThreeTest.java
@@ -0,0 +1,12 @@
+public class ThreeTest{
+    public static void main(String [] args){
+     /*   if(4>3){
+
+        }else{
+
+        }  */
+
+      String str = (4>3) ? "참" : "거짓";
+      System.out.println("str = " + str);
+    }
+}
