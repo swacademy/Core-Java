61d708b459ef11cb101980dfca29f42e63341565
swacademy <blueexpert@hotmail.com>
2018-01-10 06:18:17 +0900

    Add files via upload

---
+++ Chapter 5. Control Flow Statement/DoTest.java
@@ -0,0 +1,21 @@
+public class  DoTest{
+	public static void main(String[] args) {
+		//int i=6;
+		//while(i<=5){
+		//	System.out.print(i + "\t");
+		//	i++;
+		//}
+		/*
+			초기치;
+			do{
+                 문장;
+				 증감치;
+	        }while(조건);
+		*/
+		int i=6;
+		do{
+			System.out.print(i + "\t");
+			i++;
+		}while(i<=5);
+	}
+}
