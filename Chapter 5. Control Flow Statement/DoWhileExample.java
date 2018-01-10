0b318227dc53959066e104a67944ec97da283687
swacademy <blueexpert@hotmail.com>
2018-01-11 00:26:47 +0900

    Add files via upload

---
+++ Chapter 5. Control Flow Statement/DoWhileExample.java
@@ -0,0 +1,26 @@
+/*
+*신용권, 이것이 자바다(서울:한빛미디어, 2016), p.130
+*/
+
+import java.util.Scanner;
+
+public class  DoWhileExample {
+	public static void main(String[] args) {
+		System.out.println("메시지를 입력하세요");
+		System.out.println("프로그램을 종료하려면 q를 입력하세요.");
+		
+		Scanner scanner = new Scanner(System.in);
+		String inputString;
+		
+		do {
+			System.out.print(">");
+			inputString = scanner.nextLine();
+			System.out.println(inputString);
+		} while( ! inputString.equals("q") );
+		
+		System.out.println();
+		System.out.println("프로그램 종료");
+	}
+}
+
+
